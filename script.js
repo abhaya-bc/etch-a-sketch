228a8d6d79ad5416e768e84f3da38cda66abca7d
abhaya-bc <klmnopp@users.noreply.github.com>
2023-10-23 21:40:17 +0545

    16X16 gird square divs

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 16*16; i++) {
+    let div = document.createElement('div')
+    let text = document.createTextNode(i + 1)
+    div.setAttribute('class', 'box');
+    div.appendChild(text);
+    container.appendChild(div);
+}
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', (e)=>{
+        box.classList.add('active');
+    })
+});
